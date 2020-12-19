2366e7e0eef25050317b083e312b6b3a7fb3c04c
Ehab Salah <ehab.salah020@gmail.com>
2020-12-19 18:00:33 +0200

    failure class renamed

--- test/core/util/input_converter_test.dart
+++ test/core/util/input_converter_test.dart
@@ -1,53 +1,40 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mockito/mockito.dart';
-import 'package:mycleanarchitecture/core/error/failures.dart';
 import 'package:mycleanarchitecture/core/util/input_converter.dart';
 
-main(){
+main() {
   InputConverter inputConverter;
 
-  setUp((){
+  setUp(() {
     inputConverter = InputConverter();
-
   });
-  group("stringToUnsignedInteger", (){
-    
-    test("should return integer when the string represent unsigned integer", (){
-
+  group("stringToUnsignedInteger", () {
+    test("should return integer when the string represent unsigned integer",
+        () {
       // arrange
-      final str= '123';
+      final str = '123';
       // act
       final result = inputConverter.stringToUnsignedInteger(str);
       // assert
       expect(result, equals(Right(123)));
-
     });
 
-    test("should return a Failure when the string is not an  integer", (){
-
+    test("should return a Failure when the string is not an  integer", () {
       // arrange
-      final str= 'abc';
+      final str = 'abc';
       // act
       final result = inputConverter.stringToUnsignedInteger(str);
       // assert
-      expect(result, equals(Left(InvalidInputConverter())));
-
+      expect(result, equals(Left(InvalidInputFailure())));
     });
 
-
-    test("should return a Failure when the string is a negative integer", (){
-
+    test("should return a Failure when the string is a negative integer", () {
       // arrange
-      final str= '-1';
+      final str = '-1';
       // act
       final result = inputConverter.stringToUnsignedInteger(str);
       // assert
-      expect(result, equals(Left(InvalidInputConverter())));
-
+      expect(result, equals(Left(InvalidInputFailure())));
     });
-
-    
   });
-  
 }
